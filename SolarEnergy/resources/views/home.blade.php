764a3c30c1c0765565a42bfcc5e23add0c704d17
anam41065 <80637101+anam41065@users.noreply.github.com>
2022-06-02 16:16:05 +0100

    pagina inicio footer

--- SolarEnergy/resources/views/home.blade.php
+++ SolarEnergy/resources/views/home.blade.php
@@ -78,6 +78,8 @@
                     <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
                         been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
                         galley of type and scrambled it to make a type specimen book.</p>
+                        
+                        <!--Ver botão acertar distancias em formato desktop-->
                     <button type="button" class="btn btn-success card-button">Ver mais</button>
                 </div>
             </div>
