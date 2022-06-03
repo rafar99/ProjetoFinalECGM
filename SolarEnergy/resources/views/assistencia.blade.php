aade7915540a71eda861440ad976a223c3514ed8
anam41065 <80637101+anam41065@users.noreply.github.com>
2022-06-03 21:40:22 +0100

    pagina assistencias
    Falta meter as estrutura direita (margens laterais e embaixo)

--- SolarEnergy/resources/views/assistencia.blade.php
+++ SolarEnergy/resources/views/assistencia.blade.php
@@ -3,5 +3,100 @@
 @section('title', 'SolarEnergy')
 
 @section('content')
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/imagem1.jpg" class="d-block w-100">
+        </div>
+    </div>
 
+    <div class="container-fluid mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="titulo">
+                    <h3>Pedido de Assistência Técnica ao domicilio/empresa</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <form>
+                    <div class="form-group">
+                        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Nome">
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group mt-3">
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Rua">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group mt-3">
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Nº Porta">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group mt-3">
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Código-Postal">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group mt-3">
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Concelho">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group mt-3">
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group mt-3">
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Contacto">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <label for="exampleFormControlSelect1">Tipo Painel</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div class="form-group mt-3">
+                        <label for="exampleFormControlSelect1">Tipo de Assistência</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group mt-3">
+                                <label for="exampleFormControlSelect1">Disponibilidade do Cliente</label>
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Dia">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-groupmt-3">
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Hora">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group mt-3">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Descrição"></textarea>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
