f328b8ff793d06cf8b4017e3b2face22bd767bbf
anam41065 <80637101+anam41065@users.noreply.github.com>
2022-06-13 12:03:51 +0100

    favicon
    pagina main

--- SolarEnergy/resources/views/layouts/main.blade.php
+++ SolarEnergy/resources/views/layouts/main.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="/css/styles.css">
-
+    <link rel="icon" href="/img/logoIcon.png" type="image/x-icon">
     <style>
         /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
         html {
